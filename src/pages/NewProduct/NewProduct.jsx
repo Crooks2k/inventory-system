0ef114c4af45d07e548af98f50fad35ac2952ebd
Yochi22 <yochhii@gmail.com>
2023-09-21 22:37:37 -0500

    feat/newproduct

--- src/pages/NewProduct/NewProduct.jsx
+++ src/pages/NewProduct/NewProduct.jsx
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import Home from '../Home/Home'
-import '../NewProduct/NewProduct.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import AsideMenu from '../../core/layout/Aside/AsideMenu';
+import "../NewProduct/NewProduct.css";
+import MediaQuery from "react-responsive";
 
-function NewProduct(token) {
-  const [formData, setFormData] = useState({
-    nameProducts: '',
-    imgProduct: '',
+function NewProduct() {
+  const initialFormData = {
+    nameProducts: "",
+    imgProduct: "",
     measures: 0,
-    description: '',
+    description: "",
     price: 0,
     price2: 0,
     price3: 0,
@@ -17,232 +18,302 @@ function NewProduct(token) {
     price6: 0,
     price7: 0,
     price8: 0,
-    availability: 0,
-    category: '', // Mantén el campo como una cadena
-  });
-  console.log(formData)
+    availability: true,
+    category: "",
+    quantity: 0,
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const authToken = localStorage.getItem("token");
+
+    if (!authToken) {
+      console.error("No se encontró el token de autenticación");
+      return;
+    }
+
+    axios
+      .get("https://cugusacompany.onrender.com/api/category", {
+        headers: {
+          "x-access-token": authToken,
+        },
+      })
+      .then((response) => {
+        setCategories(response.data);
+      })
+      .catch((error) => {
+        console.error("Error al obtener las categorías:", error);
+      });
+  }, []);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
-    // Si el campo es un precio, disponibilidad o medidas, convierte el valor a un número entero
-    const newValue = (name.startsWith('price') || name === 'availability' || name === 'measures') ? parseInt(value, 10) : value;
+    let newValue;
+
+    if (name === "availability") {
+      newValue = value === "true";
+    } else if (
+      name === "quantity" ||
+      name.startsWith("price") ||
+      name === "measures"
+    ) {
+      newValue = !isNaN(value) ? parseInt(value, 10) : 0;
+    } else {
+      newValue = value;
+    }
+
     setFormData({ ...formData, [name]: newValue });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Obtener el token del localStorage
     const authToken = localStorage.getItem("token");
-    // Verifica si el token de autenticación está presente
+
     if (!authToken) {
-      console.error('No se encontró el token de autenticación');
+      console.error("No se encontró el token de autenticación");
       return;
     }
-    // Convierte la categoría en un array, dividiendo por comas
-    const categories = formData.category.split(',').map((category) => category.trim());
-   
-
-    // Cambiar formData a nivel principal
-    const requestData = {
-      formData,
-      category: categories,
-    };
 
     try {
-      // Envía la solicitud con el token de autenticación
-      await axios.post('https://cugusacompany.onrender.com/api/products/', requestData, {
-        headers: {
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json', // Asegúrate de establecer el tipo de contenido adecuado
-        },
-      });
+      await axios.post(
+        "https://cugusacompany.onrender.com/api/products/",
+        formData,
+        {
+          headers: {
+            "x-access-token": authToken,
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
-      alert('Producto registrado exitosamente');
-      // Puedes redirigir al usuario o realizar otras acciones después de enviar los datos
+      alert("Producto registrado exitosamente");
+
+      // Restablecer el estado del formulario a "initialFormData"
+      setFormData(initialFormData);
     } catch (error) {
-      console.error('Error al registrar el producto:', error);
+      console.error("Error al registrar el producto:", error);
     }
   };
 
-
-
   return (
     <>
-  <Home />
-    <div className='container-form'>
-    <h2>CREATE NEW PRODUCT</h2>
-      <form onSubmit={handleSubmit}>
-        {/* Agrega aquí los campos del formulario */}
-        <div className='campo'>
-          <label htmlFor="nameProducts">Product name:</label>
-          <input
-            type="text"
-            id="nameProducts"
-            name="nameProducts"
-            value={formData.nameProducts}
-            onChange={handleChange}
-            required
-            placeholder='Product name'
-          />
-        </div>
-       
-        <div className='campo'>
-          <label htmlFor="imgProduct">Image URL:</label>
-          <input
-            type="text"
-            id="imgProduct"
-            name="imgProduct"
-            value={formData.imgProduct}
-            onChange={handleChange}
-            required
-            placeholder='Image URL'
-          />
-        </div>
+      <div className="d-flex">
+        <aside>
+          <AsideMenu />
+        </aside>
+        <main className="w-100">
+          <div className="container-form">
+            <h2>CREATE NEW PRODUCT</h2>
+            <form onSubmit={handleSubmit}>
+              <div className="campo">
+                <label htmlFor="nameProducts">Product name:</label>
+                <input
+                  type="text"
+                  id="nameProducts"
+                  name="nameProducts"
+                  value={formData.nameProducts}
+                  onChange={handleChange}
+                  required
+                  placeholder="Product name"
+                />
+              </div>
 
-        <div className='campo'>
-          <label htmlFor="measures">Measures:</label>
-          <input
-            type="number"
-            id="measures"
-            name="measures"
-            value={formData.measures}
-            onChange={handleChange}
-            required
-          />
-        </div>
+              <div className="campo">
+                <label htmlFor="imgProduct">Image URL:</label>
+                <input
+                  type="text"
+                  id="imgProduct"
+                  name="imgProduct"
+                  value={formData.imgProduct}
+                  onChange={handleChange}
+                  required
+                  placeholder="Image URL"
+                />
+              </div>
 
-        <div className='campo'>
-          <label htmlFor="description">Description:</label>
-          <input
-            type="text"
-            id="description"
-            name="description"
-            value={formData.description}
-            onChange={handleChange}
-            required
-            placeholder='Description'
-          />
-        </div>
+              <div className="campo">
+                <label htmlFor="measures">Measures CM:</label>
+                <select
+                  className="form-select"
+                  aria-label="Default select example"
+                  id="measures"
+                  name="measures"
+                  value={formData.measures}
+                  onChange={handleChange}
+                  required
+                >
+                  <option selected>Open this select menu</option>
+                  <option value="1">2</option>
+                  <option value="2">3</option>
+                </select>
+              </div>
 
-        <div className='campo'>
-          <label htmlFor="price">Price West Palm Beach:</label>
-          <input
-            type="number"
-            id="price"
-            name="price"
-            value={formData.price}
-            onChange={handleChange}
-          />
-        </div>
+              <div className="campo">
+                <label htmlFor="description">Description:</label>
+                <input
+                  type="text"
+                  id="description"
+                  name="description"
+                  value={formData.description}
+                  onChange={handleChange}
+                  required
+                  placeholder="Description"
+                />
+              </div>
 
-        <div className='campo'>
-          <label htmlFor="price2">Price Stuart</label>
-          <input
-            type="number"
-            id="price2"
-            name="price2"
-            value={formData.price2}
-            onChange={handleChange}
-          />
-        </div>
+              <div className="campo">
+                <label htmlFor="price">Price West Palm Beach:</label>
+                <input
+                  type="number"
+                  id="price"
+                  name="price"
+                  value={formData.price}
+                  onChange={handleChange}
+                />
+              </div>
 
-        <div className='campo'>
-          <label htmlFor="price3">Price Daytona Beach</label>
-          <input
-            type="number"
-            id="price3"
-            name="price3"
-            value={formData.price3}
-            onChange={handleChange}
-          />
-        </div>
+              <div className="campo">
+                <label htmlFor="price2">Price Stuart</label>
+                <input
+                  type="number"
+                  id="price2"
+                  name="price2"
+                  value={formData.price2}
+                  onChange={handleChange}
+                />
+              </div>
 
-        <div className='campo'>
-          <label htmlFor="price4">Price Jacksonville</label>
-          <input
-            type="number"
-            id="price4"
-            name="price4"
-            value={formData.price4}
-            onChange={handleChange}
-          />
-        </div>
+              <div className="campo">
+                <label htmlFor="price3">Price Daytona Beach</label>
+                <input
+                  type="number"
+                  id="price3"
+                  name="price3"
+                  value={formData.price3}
+                  onChange={handleChange}
+                />
+              </div>
 
-        <div className='campo'>
-          <label htmlFor="price5">Price Houston</label>
-          <input
-            type="number"
-            id="price5"
-            name="price5"
-            value={formData.price5}
-            onChange={handleChange}
-          />
-        </div>
+              <div className="campo">
+                <label htmlFor="price4">Price Jacksonville</label>
+                <input
+                  type="number"
+                  id="price4"
+                  name="price4"
+                  value={formData.price4}
+                  onChange={handleChange}
+                />
+              </div>
 
-        <div className='campo'>
-          <label htmlFor="price6">Price Dallas</label>
-          <input
-            type="number"
-            id="price6"
-            name="price6"
-            value={formData.price6}
-            onChange={handleChange}
-          />
-        </div>
+              <div className="campo">
+                <label htmlFor="price5">Price Houston</label>
+                <input
+                  type="number"
+                  id="price5"
+                  name="price5"
+                  value={formData.price5}
+                  onChange={handleChange}
+                />
+              </div>
 
-        <div className='campo'>
-          <label htmlFor="price7">Price Beaumont</label>
-          <input
-            type="number"
-            id="price7"
-            name="price7"
-            value={formData.price7}
-            onChange={handleChange}
-            required
-          />
-        </div>
+              <div className="campo">
+                <label htmlFor="price6">Price Dallas</label>
+                <input
+                  type="number"
+                  id="price6"
+                  name="price6"
+                  value={formData.price6}
+                  onChange={handleChange}
+                />
+              </div>
 
-        <div className='campo'>
-          <label htmlFor="price8">Price New Orleans</label>
-          <input
-            type="number"
-            id="price8"
-            name="price8"
-            value={formData.price8}
-            onChange={handleChange}
-          />
-        </div>
+              <div className="campo">
+                <label htmlFor="price7">Price Beaumont</label>
+                <input
+                  type="number"
+                  id="price7"
+                  name="price7"
+                  value={formData.price7}
+                  onChange={handleChange}
+                />
+              </div>
 
-        <div className='campo'>
-          <label htmlFor="availability">Availability:</label>
-          <input
-            type="number"
-            id="availability"
-            name="availability"
-            value={formData.availability}
-            onChange={handleChange}
-            required
-          />
-        </div>
+              <div className="campo">
+                <label htmlFor="price8">Price New Orleans</label>
+                <input
+                  type="number"
+                  id="price8"
+                  name="price8"
+                  value={formData.price8}
+                  onChange={handleChange}
+                />
+              </div>
 
-        <div className='campo'>
-          <label htmlFor="category">Category:</label>
-          <input
-            type="text"
-            id="category"
-            name="category"
-            value={formData.category}
-            onChange={handleChange}
-            required
-            placeholder='Category'
-          />
-        </div>
-      <button className='button' type="submit">Add new product</button>
-      </form>
-      
-    </div>
+              <div className="campo">
+                <label htmlFor="quantity">Cantidad</label>
+                <input
+                  type="number"
+                  id="quantity"
+                  name="quantity"
+                  value={formData.quantity}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div className="campo">
+                <label htmlFor="availability">Availability:</label>
+                <select
+                  className="form-select"
+                  aria-label="Default select example"
+                  id="availability"
+                  name="availability"
+                  value={formData.availability.toString()}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="true">Yes</option>
+                  <option value="false">No</option>
+                </select>
+              </div>
+
+              <div className="campo">
+                <label htmlFor="category">Category:</label>
+                <select
+                  className="form-select"
+                  aria-label="Default select example"
+                  id="category"
+                  name="category"
+                  value={formData.category}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="" disabled>
+                    Select a category
+                  </option>
+                  {Array.isArray(categories) && categories.length > 0 ? (
+                    categories.map((category) => (
+                      <option key={category._id} value={category.filter}>
+                        {category.filter}
+                      </option>
+                    ))
+                  ) : (
+                    <option value="" disabled>
+                      No categories available
+                    </option>
+                  )}
+                </select>
+              </div>
+              <button className="button" type="submit">
+                Add new product
+              </button>
+            </form>
+          </div>
+        </main>
+      </div>
     </>
   );
 }
-
 export default NewProduct;
